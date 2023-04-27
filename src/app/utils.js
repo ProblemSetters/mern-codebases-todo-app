2a01b5d53104562672f6a1c6e128b7d7c1f5fa05
varadp27 <varad.patil@hackerrank.com>
2023-04-27 12:19:04 +0530

    fix: not detecting port 8000 with baseURLGenerator

--- src/app/utils.js
+++ src/app/utils.js
@@ -1,9 +1,10 @@
 export const generateBaseURL = () => {
   const currentHost = window?.location?.host || "localhost:8001";
   const currentProtocol = window?.location?.protocol || "http:";
-  const baseURL = `${currentProtocol}//${currentHost.replace(
-    "8001",
+	const baseURL = `${currentProtocol}//${currentHost.replace(
+	currentHost.match(/8001/)?"8001": "8000",
     "8080"
   )}/api/todo`;
+	console.log(baseURL)
   return baseURL;
 };
